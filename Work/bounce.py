db27fcb3220152390bb930dde1dd1d7a37267bfd
Anna Lowegard <alowegard@Annas-MacBook-Pro.local>
2022-06-14 13:42:21 +0100

    Updating what I've finished so far.

--- Work/bounce.py
+++ Work/bounce.py
@@ -1,3 +1,10 @@
 # bounce.py
 #
 # Exercise 1.5
+ball_height = 100 # ball height in meters
+bounce = 1 # number of bounces
+
+while bounce < 11:
+    ball_height = ball_height * 0.6 # ball height changes to 3/5 the current height
+    print(bounce,round(ball_height,4)) # print the bounce number and the current height
+    bounce += 1 
--- Work/mortgage.py
+++ Work/mortgage.py
@@ -1,3 +1,30 @@
 # mortgage.py
 #
 # Exercise 1.7
+
+principal = 500000.0
+rate = 0.05
+payment = 2684.11
+total_paid = 0.0
+month = 0
+
+extra_payment = 2000.0
+extra_payment_start_month = 61
+extra_payment_end_month = 108
+
+while principal > payment:
+    month += 1
+    principal = principal * (1+rate/12) - payment
+    total_paid += payment 
+    
+    if month <= extra_payment_end_month and month >= extra_payment_start_month and principal >= extra_payment:
+        principal = principal - extra_payment
+        total_paid += extra_payment
+    
+    print(month, round(total_paid,2), round(principal,2))
+
+if principal > 0:
+    month += 1
+    total_paid += principal
+    print(month, round(total_paid,2), 0.0)
+
